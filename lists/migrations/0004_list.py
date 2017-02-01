7438733e6239c0bf92522045fd8c3a4c41779324
MN on zverjasec <novakmitja@gmail.com>
2017-02-01 21:26:53 +0100

    ponovni commit

---
+++ lists/migrations/0004_list.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('lists', '0003_auto_20170126_2346'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='List',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+            ],
+        ),
+    ]
